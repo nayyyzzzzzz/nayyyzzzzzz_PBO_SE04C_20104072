e4931310c906e2229ee4931f67105975a341e67a
nayyyzzzzz <naylanisa291@gmail.com>
2021-10-17 17:51:42 +0700

    Revert "modul02 for tugas"
    This reverts commit 5914d402553c50f35a69ca3c0f6589035eb28b38.

--- naiii/latihan/batas_maksimum.java
+++
@@ -1,9 +0,0 @@
-package com.naiii.latihan;
-import java.util.Scanner;
-public class batas_maksimum {
-    public static void main(String[] args) {
-        long p = 2147483648L;
-
-        System.out.println(p);
-    }
-}
