725ede086720e3ec2b2db9005d810982221bef3e
nayyyzzzzz <naylanisa291@gmail.com>
2021-10-17 10:34:02 +0700

    modul 2 for tugas

---
+++ com/naiii/latihan/batas_maksimum.java
@@ -0,0 +1,9 @@
+package com.naiii.latihan;
+import java.util.Scanner;
+public class batas_maksimum {
+    public static void main(String[] args) {
+        long p = 2147483648L;
+
+        System.out.println(p);
+    }
+}
